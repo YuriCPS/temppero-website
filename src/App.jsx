d45dc971c246d78eb03b1a2d0100fdb960f83545
Yuri Carvalho <yuricps@gmail.com>
2022-04-16 03:07:14 -0300

    feat: create Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from './components/Button';
 import logo from './images/logo.png';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <h1>Temppero</h1>
       <img src={logo} alt="Temppero" />
+      <Button />
     </>
   );
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Button() {
+  return (
+    <button type="button">
+      CLIQUE AQUI E FAÇA SEU PEDIDO
+    </button>
+  );
+}
+
+export default Button;
