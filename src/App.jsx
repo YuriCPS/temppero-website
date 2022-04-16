9a4387db76dcb14bed6d2b32c501c90960b2800c
Yuri Carvalho <yuricps@gmail.com>
2022-04-16 03:03:20 -0300

    feat: add logo and background color

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import logo from './images/logo.png';
 
 function App() {
   return (
-    <h1>Temppero</h1>
+    <>
+      <h1>Temppero</h1>
+      <img src={logo} alt="Temppero" />
+    </>
   );
 }
 
