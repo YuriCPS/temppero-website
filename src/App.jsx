d46352c2f6c2ef6708cf8b466dabdc20a9dce237
Yuri Carvalho <yuricps@gmail.com>
2022-04-18 17:38:07 -0300

    feat: add banners

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import Banner from './components/Banner';
 import Button from './components/Button';
 import logo from './images/logo.png';
-import nossaComida from './images/01.webp';
+import nossaComidaImg from './images/01.webp';
+import tempperoImg from './images/02.webp';
+import desdeImg from './images/03.webp';
 
 function App() {
   return (
@@ -20,7 +22,17 @@ function App() {
       <Banner
         title="Nossa Comida"
         text="Preparamos nossos pratos com os melhores ingredientes encontrados no mercado, zelando pela qualidade e sabor oferecido a nosso cliente. As melhores marcas e fornecedores para proporcionar a você o que há de melhor."
-        img={nossaComida}
+        img={nossaComidaImg}
+      />
+      <Banner
+        title="Temppero"
+        text="De família com descendência italiana, a Chef sempre gostou de cozinhar e se dedicar a comida, com mais de 15 anos trabalhando diretamente no ramo de alimentação seja com seu próprio negócio ou não, todo alimento preparado por nós tem o 'temppero' tradicional que todos que provam reconhecem."
+        img={tempperoImg}
+      />
+      <Banner
+        title="Desde 2004"
+        text="Começamos com uma lanchonete de nome Big Boca, na mesma localidade onde funcionamos hoje. Com o tempo fomos crescendo e ampliando nosso cardápio até oferecer almoço, com o mesmo sabor caseiro que você se delicia hoje. Encerramos nossas atividades no ano de 2012, retornando em 2017 com novo nome, desta vez Temppero."
+        img={desdeImg}
       />
     </>
   );
