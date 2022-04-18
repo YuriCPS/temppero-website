f92dfd9122450342b3096f9baa976a25e7f0d714
Yuri Carvalho <yuricps@gmail.com>
2022-04-18 18:03:39 -0300

    feat: adjust Banner css to mobile first

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,10 +3,10 @@ import { string } from 'prop-types';
 
 function Banner({ title, text, img }) {
   return (
-    <div className="flex m-6 flex-col sm:flex-row">
-      <div className="bg-white p-4 rounded-t-md sm:rounded-l-md">
-        <h2 className="text-3xl font-bold">{title}</h2>
-        <p className="my-6 text-xl">{text}</p>
+    <div className="flex m-6 flex-col sm:flex-row sm:mx-10 md:mx-20 lg:mx-40 xl:mx-80">
+      <div className="bg-white p-4 rounded-t-md sm:rounded-l-md grid">
+        <h2 className="text-3xl font-bold sm:text-4xl">{title}</h2>
+        <p className="my-6 text-xl sm:text-2xl">{text}</p>
       </div>
       <img
         className="rounded-b-md sm:rounded-r-md"
